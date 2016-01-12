8520c2481d1c606a7777bba41969bf814b2be7e5
Daniel Haim <danielmoisehaim@gmail.com>
2016-01-12 22:23:45 +0200

    Dernier changement

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,9 +96,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATIC_URL = '/static/'
